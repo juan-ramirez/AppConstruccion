5482ab083a3021178640582528bd58f8190528c9
Mixer <mixergutierrez@gmail.com>
2013-12-23 01:54:00 -0500

    Listos los layouts principales

--- AppConstruccion/src/com/movilapps/appconstruccion/MainActivity.java
+++ AppConstruccion/src/com/movilapps/appconstruccion/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main_proyectos_formularios);
 	}
 
 	@Override
